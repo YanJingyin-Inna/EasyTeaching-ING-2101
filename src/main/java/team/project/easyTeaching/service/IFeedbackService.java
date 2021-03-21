cae5c8dff427221a39f19f16ade4348cd26747da
Teaching_test <teaching-test@163.com>
2021-03-21 19:37:47 +0800

    删除feedback相关类

--- src/main/java/team/project/easyTeaching/service/IFeedbackService.java
+++
@@ -1,8 +0,0 @@
-package team.project.easyTeaching.service;
-
-import team.project.easyTeaching.common.ServerResponse;
-import team.project.easyTeaching.pojo.FeedbackInfo;
-
-public interface IFeedbackService {
-    ServerResponse<String> UploadInfo(FeedbackInfo feedbackInfo);
-}
