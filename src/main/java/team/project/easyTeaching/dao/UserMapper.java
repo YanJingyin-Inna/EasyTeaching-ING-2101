ab2089bceb8737a0ed862cf690663890f2e3f6a3
郑素华 <1797244807@qq.com>
2021-03-21 09:44:05 +0800

    UserMapper

---
+++ src/main/java/team/project/easyTeaching/dao/UserMapper.java
@@ -0,0 +1,35 @@
+package team.project.easyTeaching.dao;
+
+import org.apache.ibatis.annotations.Param;
+import team.project.easyTeaching.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer uid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer uid);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    int checkUsername(String username);
+
+    int checkEmail(String email);
+
+    User selectLogin(@Param("username") String username,@Param("password")String password);
+
+    String selectQuestionByUsername(String username);
+
+    int checkAnswer(@Param("username")String username,@Param("question")String question,@Param("answer")String answer);
+
+    int updatePasswordByUsername(@Param("username")String username,@Param("passwordNew")String passwordNew);
+
+    int checkPassword(@Param("password")String password,@Param("uid")String uid);
+
+    int checkEmailByUserId(@Param(value="email")String email,@Param(value="userId")Integer userId);
+
+}
